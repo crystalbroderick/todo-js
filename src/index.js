24a30494e646de9b8bc148eb00fdd5904f99aabf
crystalbroderick <broderick.crystal@gmail.com>
2025-01-01 17:58:37 -0600

    init webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import logger from "./utils";
+import {saveStorage, getStorage} from "./storage.js"
+import dom from './dom.js';
+
+import {projectManager} from "./projects.js"
+import "./styles.css"
+
+dom.renderProjects()
