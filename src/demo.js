9cadd2306f65f63b5fc469123befff35fae42b0c
crystalbroderick <broderick.crystal@gmail.com>
2025-01-01 17:59:58 -0600

    mock data for default project

---
+++ src/demo.js
@@ -0,0 +1,16 @@
+const demo = [
+  {
+    id: Date.now(),
+    title: "Default",
+    tasks: [
+      {
+        title: "Create to do list!",
+        desc: "Work on using es6 modules and classes",
+        priority: 2,
+        completed: false,
+      },
+    ],
+  },
+]
+
+export default demo
