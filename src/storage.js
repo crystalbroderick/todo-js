12ab4a6209a4e71c682e3e74cf24fa724a031c51
crystalbroderick <broderick.crystal@gmail.com>
2025-01-01 18:00:18 -0600

    get and save local storage

---
+++ src/storage.js
@@ -0,0 +1,14 @@
+import logger from "./utils"
+
+function saveStorage(key, value) {
+  logger("saving to storage..")
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+function getStorage(key) {
+  logger("loading from storage..")
+  const data = localStorage.getItem(key)
+  return data ? JSON.parse(data) : null
+}
+
+export { saveStorage, getStorage }
