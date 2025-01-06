7be17cb6de464f6b3e744fd92968ff94dfcb3ac0
crystalbroderick <broderick.crystal@gmail.com>
2025-01-06 02:53:15 -0600

    more demo projects with tasks for testing

--- src/demo.js
+++ src/demo.js
@@ -4,10 +4,34 @@ const demo = [
     title: "Default",
     tasks: [
       {
-        title: "Create to do list!",
-        desc: "Work on using es6 modules and classes",
+        id: 1,
+        title: "Groceries",
+        desc: "Lemon pepper, coffee, milk, eggs",
+        priority: 1,
+        completed: false,
+        dueDate: Date.now()
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Coding Project",
+    tasks: [
+      {
+        id: 1,
+        title: "add tasks to project",
+        desc: "create task button, form, styles..",
         priority: 2,
         completed: false,
+        dueDate: Date.now()
+      },
+      {
+        id: 2,
+        title: "delete project",
+        desc: "add delete functionality with button and remove one project when user clicks.",
+        priority: 3,
+        completed: false,
+        dueDate: Date.now()
       },
     ],
   },
