6439e915ef5eae3582ad427296dd9e6d60e41644
crystalbroderick <broderick.crystal@gmail.com>
2025-01-16 20:11:45 -0600

    update tasks

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,6 @@
-import logger, { capitalize } from "./utils"
+import logger from "./utils"
 import demo from "./demo"
 import { render } from "./dom"
-import Task from "./tasks"
 
 const { getStorage, saveStorage, clearStorage } = require("./storage")
 
@@ -13,8 +12,6 @@ const projectManager = (function () {
     activeProjectId = projects[0].id
     saveStorage("activeProjectId", activeProjectId)
   }
-
-
 
   const initializeProjects = () => {
     if (projects.length === 0) {
@@ -40,7 +37,8 @@ const projectManager = (function () {
     saveStorage("activeProjectId", activeProjectId)
   }
 
-  const getActiveProject = () => projects.find((project) => project.id === activeProjectId)
+  const getActiveProject = () =>
+    projects.find((project) => project.id === activeProjectId)
 
   const getActiveProjectId = () => activeProjectId
 
@@ -59,15 +57,22 @@ const projectManager = (function () {
     }
   }
 
-
   const updateTask = (newInfo) => {
     const project = getActiveProject()
-    logger(newInfo)
-    // const taskToUpdate = project.tasks.find(obj => obj.id === newInfo.id);
-    // if(taskToUpdate) {
-    //   taskToUpdate.title = newInfo.title;
-    //   save()
-    // }
+    console.log(newInfo)
+    const taskToUpdate = project.tasks.find((obj) => obj.id === newInfo.id)
+
+    if (taskToUpdate) {
+      // Update the task's properties with the new information
+      Object.keys(newInfo).forEach((key) => {
+        if (key in taskToUpdate) {
+          taskToUpdate[key] = newInfo[key]
+        }
+      })
+      save()
+    }
+    logger("Updated task:", newInfo);
+
   }
 
   // const getCompletedTasks = () => activeProject.tasks.filter(task=>task.completed)
@@ -76,24 +81,20 @@ const projectManager = (function () {
 
   // addTaskToProject(testTask)
 
-  
-
   const removeTask = (taskId) => {
     let p = getActiveProject()
-    p.tasks = p.tasks.filter(task => task.id !== taskId);
+    p.tasks = p.tasks.filter((task) => task.id !== taskId)
   }
 
   const deleteProject = (projectId) => {
-    projects = projects.filter(p => p.id !== projectId);
-    }
+    projects = projects.filter((p) => p.id !== projectId)
+  }
 
   const deleteProjects = () => {
     clearStorage()
     initializeProjects()
     render.renderContent()
   }
-
-
 
   return {
     initializeProjects,
@@ -103,14 +104,17 @@ const projectManager = (function () {
     getActiveProjectId,
     getActiveProject,
     deleteProjects,
-    save, 
-    addTaskToProject, removeTask, updateTask, deleteProject
+    save,
+    addTaskToProject,
+    removeTask,
+    updateTask,
+    deleteProject,
   }
 })()
 
 export function createProject(title) {
   return {
-    id: Date.now().toString(),
+    id: Date.now(),
     title,
     tasks: [],
     // toggleComplete() {
@@ -118,8 +122,5 @@ export function createProject(title) {
     // },
   }
 }
-
-
-
 
 export { projectManager }
